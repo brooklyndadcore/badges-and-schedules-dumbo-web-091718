274f395ef7451276f2ab4a56b938891451d89ca1
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-09 20:51:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+$array_names = []
+def batch_badge_creator(array = [])
+  new_arr = []
+  array.each {|ele| $array_names << ele}
+  array.each {|ele| new_arr << "Hello, my name is #{ele}."}
+  return new_arr
+end
+
+def assign_rooms
+  new_arr2 = []
+  $array_names.each_with_index do |ele, idx|
+    new_arr2 << "Hello, #{ele}! You'll be assigned to room #{idx+1}!"
+  end
+  return new_arr2
+end
+
+def printer
+  puts batch_badge_creator(array)
+  puts assign_rooms
+end
