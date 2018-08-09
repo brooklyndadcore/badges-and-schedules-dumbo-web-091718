48b6ebc754bd4b8075e8e755fb6a4d1fae11312d
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-09 21:07:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,9 +20,7 @@ def assign_rooms(array = $array_names)
 end
 
 def printer(attendees)
-  assign_rooms.each do |ele|
-    puts ele
-  end
+
   batch_badge_creator(attendees).each do |ele|
     puts ele
   end
